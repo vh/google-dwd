db19c820d9f3edd9dd5190676d17d524f0fcd74b
Vadim Homchik <homchik@gmail.com>
2022-07-28 16:10:06 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # google-dwd
+Google Domain Wide Delegation Client.
+
+## Usage
+```js
+const { GoogleAuth } = require('google-auth-library');
+const dwd = require('google-dwd');
+
+const auth = new GoogleAuth();
+const client = await auth.getClient();
+
+const client = await dwd(client, 'test@googleworkspace.com', [ 'https://www.googleapis.com/auth/gmail.readonly' ]);
+```
---
+++ src/index.js
@@ -0,0 +1,67 @@
+const { Compute, Impersonated, JWT, OAuth2Client } = require('google-auth-library');
+
+const { request } = require('gaxios');
+
+const google = require('@googleapis/iam');
+
+const TOKEN_ENDPOINT = 'https://oauth2.googleapis.com/token';
+
+const unpaddedB64encode = (input) =>
+  Buffer.from(input)
+    .toString('base64')
+    .replace(/=*$/, '');
+
+module.exports = async (client, subject, scopes) => {
+  let iss;
+  if (client instanceof JWT) {
+    iss = client.email;
+  } else if (client instanceof Compute) {
+    iss = client.serviceAccountEmail;
+  } else if (client instanceof Impersonated) {
+    iss = client.targetPrincipal;
+  }
+
+  if (iss) {
+    const now = Math.floor(new Date().getTime() / 1000);
+    const expiry = now + 3600;
+    const payload = JSON.stringify({
+      aud: TOKEN_ENDPOINT,
+      exp: expiry,
+      iat: now,
+      iss: iss,
+      scope: scopes.join(' '),
+      sub: subject,
+    });
+
+    const header = JSON.stringify({
+      alg: 'RS256',
+      typ: 'JWT',
+    });
+
+    const iamPayload = `${unpaddedB64encode(header)}.${unpaddedB64encode(payload)}`;
+
+    const iam = google.iam('v1');
+    const { data } = await iam.projects.serviceAccounts.signBlob({
+      auth: client,
+      name: `projects/-/serviceAccounts/${iss}`,
+      requestBody: {
+        bytesToSign: unpaddedB64encode(iamPayload)
+      },
+    });
+
+    const assertion = `${iamPayload}.${data.signature.replace(/=*$/, '')}`;
+
+    const headers = { 'content-type': 'application/x-www-form-urlencoded' };
+    
+    const body = new URLSearchParams({ assertion, grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer' }).toString();
+
+    const response = await request({ url: TOKEN_ENDPOINT, method: 'POST', headers, body });
+
+    const newCredentials = new OAuth2Client();
+    newCredentials.setCredentials({ access_token: response.data.access_token });
+
+    return newCredentials;
+  } else {
+    throw new Error('Unexpected authentication type');
+  }
+}
