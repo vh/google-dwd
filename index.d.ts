a72b004399521c677b46dd6671db104981cb551a
Vadim Homchik <homchik@gmail.com>
2022-10-18 11:08:47 +0300

    Allow JSONClient

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 import { OAuth2Client } from "google-auth-library";
+import { JSONClient } from "google-auth-library/build/src/auth/googleauth";
 
-export default function(client: OAuth2Client, subject: string, scopes: string[]): Promise<OAuth2Client>;
+export default function(client: OAuth2Client | JSONClient, subject: string, scopes: string[]): Promise<OAuth2Client>;
